ac78b13979f817d6d221e3b96c51a5066eaa212f
raedaltaki <raed.simon@gmail.com>
2021-03-19 14:40:15 -0400

    add signup - login - logout methods

---
+++ public/javascript/login.js
@@ -0,0 +1,107 @@
+async function loginBtnHandler(event) 
+{
+    event.preventDefault();
+
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if(username && password)
+    {
+        const res= await fetch("/api/users/login",
+            {
+                method: "post",
+                body: JSON.stringify(
+             {
+                        email,
+                        password
+                    }
+                ),
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        if(res.ok)
+        {
+            document.location.replace('/dashboard');
+        } 
+        else 
+        {
+            window.alert("please check your username and password");
+            alert(response.statusText);
+        }
+    }
+}
+
+async function signupBtnHandler(event) 
+{
+    event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    if(username && email && password)
+    {
+        const res= await fetch("/api/users/",
+            {
+                method: "post",
+                body: JSON.stringify(
+             {
+                        username,
+                        email,
+                        password
+                    }
+                ),
+                headers:
+                {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+
+        if(res.ok)
+        {
+            const res= await fetch("/api/users/login",
+{
+                    method: "post",
+                    body: JSON.stringify(
+                 {
+                            email,
+                            password
+                        }
+                    ),
+                    headers:
+                    {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+
+            if(res.ok)
+            {
+                document.location.replace('/dashboard');
+            } 
+            else 
+            {
+                alert(response.statusText);
+            }
+        } 
+        else 
+        {
+            alert(response.statusText);
+        }
+    }
+}
+
+
+if(document.getElementById('login-form'))
+{
+    document.getElementById('login-form').addEventListener("submit",loginBtnHandler);
+}
+else
+{
+    document.getElementById('signup-form').addEventListener("submit",signupBtnHandler);
+}
